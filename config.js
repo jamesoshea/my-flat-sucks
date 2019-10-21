ff93e359c3c7d11750af767be447878d49c6543c
James O'Shea <j.oshea@holidaypirates.com>
2019-10-21 20:42:28 +0200

    feat: grab urls of apartments which are not in Spandau

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  minimumFloorSpace: 50,
+  searchCenter: {
+    latitude: 52.521641,
+    longitude: 13.40342,
+    maxDistance: 6000,
+  },
+};
--- puppeteer.js
+++ puppeteer.js
@@ -1,12 +1,16 @@
 require('dotenv').config();
 const axios = require('./axios');
-const puppeteer = require('puppeteer');
 const fs = require('fs');
+const { getDistance } = require('geolib');
+const puppeteer = require('puppeteer');
 
 const password = process.env.PASSWORD;
 const userId = process.env.USER_ID;
 
-const oldIds = JSON.parse(fs.readFileSync('ids.json'));
+const { searchCenter, minimumFloorSpace } = require('./config');
+
+const oldIds = [];
+// const oldIds = JSON.parse(fs.readFileSync('ids.json'));
 
 (async () => {
   try {
@@ -31,20 +35,37 @@ const oldIds = JSON.parse(fs.readFileSync('ids.json'));
     const idsFromPage = await page.$$eval('.result-list__listing', (items) =>
       items.map((item) => item.dataset.id),
     );
-    const set = Array.from(new Set([...oldIds, ...idsFromPage]));
-    const newIds = idsFromPage.filter((id) => oldIds.includes(id));
-    newIds.forEach((id) => {
-      axios
-        .get(
-          `https://rest.immobilienscout24.de/restapi/api/search/v1.0/expose/${id}`,
-        )
-        .then((res) => console.log(res.data['expose.expose']['@id']))
-        .catch((err) => console.log(err.response.data));
-    });
 
+    const allExistingIds = Array.from(new Set([...oldIds, ...idsFromPage]));
     // save offer ids so we don't message anyone twice
-    fs.writeFileSync('ids.json', JSON.stringify(set));
-    await page.screenshot({ path: 'example.png' });
+    fs.writeFileSync('ids.json', JSON.stringify(allExistingIds));
+
+    const newIds = idsFromPage.filter((id) => !oldIds.includes(id));
+    newIds.forEach(async (id) => {
+      const { data } = await axios.get(
+        `https://rest.immobilienscout24.de/restapi/api/search/v1.0/expose/${id}`,
+      );
+      const coords = data['expose.expose'].realEstate.address.wgs84Coordinate;
+      const hasBalcony = data['expose.expose'].realEstate.balcony;
+      const isCloseEnough =
+        getDistance(
+          {
+            latitude: searchCenter.latitude,
+            longitude: searchCenter.longitude,
+          },
+          {
+            latitude: coords.latitude,
+            longitude: coords.longitude,
+          },
+        ) < searchCenter.maxDistance;
+      const isBigEnough =
+        Number(data['expose.expose'].realEstate.usableFloorSpace) >
+        minimumFloorSpace;
+      if (isBigEnough && isCloseEnough && hasBalcony) {
+        console.log(`https://www.immobilienscout24.de/expose/${id}`);
+      }
+    });
+    // await page.screenshot({ path: 'debug.png' });
     await browser.close();
   } catch (err) {
     console.log(err);
