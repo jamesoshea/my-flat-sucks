ccf4ae10b3e77b6fb6bdb16b057da6322ec48793
James O'Shea <jamesoshea89@hotmail.com>
2020-02-16 13:41:24 +0100

    fix browser opening logic

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const puppeteer = require('puppeteer');
 const axios = require('./createOAuthRequest');
 const { createMessage, isApartmentGood } = require('./utils');
 
-const password = process.env.PASSWORD;
-const userId = process.env.USER_ID;
+const { DEBUG, PASSWORD, USER_ID } = process.env;
 
 const main = async () => {
   console.log(`spinning up at ${new Date().toString()}`);
   const browser = await puppeteer.launch({ headless: false });
+  browser.on('disconnected', () => {
+    console.log('browser closed');
+  });
   try {
     const page = await browser.newPage();
     page.setViewport({
@@ -21,10 +23,10 @@ const main = async () => {
 
     // attempt to login
     await page.goto(
-      `https://sso.immobilienscout24.de/sso/login?appName=is24main&source=meinkontodropdown-login&sso_return=https://www.immobilienscout24.de/sso/login.go?source%3Dmeinkontodropdown-login%26returnUrl%3D/geschlossenerbereich/start.html?source%253Dmeinkontodropdown-login&u=${userId}=&nl=true`,
+      `https://sso.immobilienscout24.de/sso/login?appName=is24main&source=meinkontodropdown-login&sso_return=https://www.immobilienscout24.de/sso/login.go?source%3Dmeinkontodropdown-login%26returnUrl%3D/geschlossenerbereich/start.html?source%253Dmeinkontodropdown-login&u=${USER_ID}=&nl=true`,
     );
 
-    await page.type('#password', password);
+    await page.type('#password', PASSWORD);
     await page.click('#loginOrRegistration');
 
     // go to page and fetch offers
@@ -46,52 +48,62 @@ const main = async () => {
 
     // save offer ids so we don't message anyone twice
     fs.writeFileSync('ids.json', JSON.stringify([...newIds, ...keys]));
-
-    const promiseArray = newIds.map(async (id) => {
-      new Promise((resolve, reject) => {
-        const url = `https://rest.immobilienscout24.de/restapi/api/search/v1.0/expose/${id}`;
-        try {
-          axios.get(url).then(async ({ data }) => {
-            const property = {
-              coords: data['expose.expose'].realEstate.address.wgs84Coordinate,
-              hasBalcony: data['expose.expose'].realEstate.balcony,
-              floorSpace: data['expose.expose'].realEstate.livingSpace,
-              postcode: data['expose.expose'].realEstate.address.postcode,
-            };
-            const messageParameters = {
-              lastName: data['expose.expose'].contactDetails.lastname,
-              salutation: data['expose.expose'].contactDetails.salutation,
-              street: data['expose.expose'].realEstate.address.street,
-            };
-            const message = createMessage(messageParameters);
-            if (!isApartmentGood(property)) {
-              console.log('skipped', id);
+    const promiseArray = [];
+    newIds.forEach(async (id) => {
+      promiseArray.push(
+        new Promise((resolve, reject) => {
+          const url = `https://rest.immobilienscout24.de/restapi/api/search/v1.0/expose/${id}`;
+          try {
+            axios.get(url).then(async ({ data }) => {
+              const property = {
+                coords:
+                  data['expose.expose'].realEstate.address.wgs84Coordinate,
+                hasBalcony: data['expose.expose'].realEstate.balcony,
+                floorSpace: data['expose.expose'].realEstate.livingSpace,
+                postcode: data['expose.expose'].realEstate.address.postcode,
+              };
+              const messageParameters = {
+                lastName: data['expose.expose'].contactDetails.lastname,
+                salutation: data['expose.expose'].contactDetails.salutation,
+                street: data['expose.expose'].realEstate.address.street,
+              };
+              const message = createMessage(messageParameters);
+              if (!isApartmentGood(property)) {
+                console.log('skipped', id);
+                resolve();
+                return;
+              }
+              const apartmentDetailPage = await browser.newPage();
+              apartmentDetailPage.setViewport({
+                width: 1280,
+                height: 720,
+                deviceScaleFactor: 1,
+              });
+              await apartmentDetailPage.goto(
+                `https://www.immobilienscout24.de/expose/${id}`,
+                {
+                  timeout: 0,
+                },
+              );
+              await apartmentDetailPage.click('[data-qa="sendButton"]');
+              await apartmentDetailPage.waitForSelector('textarea', {
+                visible: true,
+              });
+              await apartmentDetailPage.type('textarea', message);
+              if (!DEBUG) {
+                await page.click('[data-qa="sendButtonBasic"]');
+              }
+              await apartmentDetailPage.close();
+              console.log('done', id);
               resolve();
-              return;
-            }
-            const page = await browser.newPage();
-            page.setViewport({
-              width: 1280,
-              height: 720,
-              deviceScaleFactor: 1,
             });
-            await page.goto(`https://www.immobilienscout24.de/expose/${id}`, {
-              timeout: 0,
-            });
-            await page.click('[data-qa="sendButton"]');
-            await page.waitForSelector('textarea', { visible: true });
-            await page.type('textarea', message);
-            // await page.click('[data-qa="sendButtonBasic"]');
-            await page.close();
-            console.log('done', id);
-            resolve();
-          });
-        } catch (error) {
-          reject(error);
-        }
-      });
+          } catch (error) {
+            reject(error);
+          }
+        }),
+      );
     });
-    await Promise.allSettled(promiseArray);
+    await Promise.allSettled(promiseArray).catch((error) => console.log(error));
     console.log(`closing crawler at ${new Date().toString()}`);
     await browser.close();
   } catch (err) {
